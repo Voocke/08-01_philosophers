18ebd6c17b4efc1c92f1d2abce7a7b11a7222c7b
ivan <timofeevi579@gmail.com>
2025-12-14 16:37:49 +0300

    almost

--- src/Main.kt
+++ src/Main.kt
@@ -2,91 +2,73 @@ import kotlin.random.Random
 
 fun main() {
     println("Введите кол-во философов: ")
-    var n = readln().toInt()
+    val n = readln().toInt()
 
-    val forks: MutableList<Boolean> = MutableList(5) {true}
-
+    val forks: MutableList<Boolean> = MutableList(n) {true}
     val thinkers: List<Thinker> = List(n) {Thinker(Status.WAIT)}
 
-    var startThinker = Random.nextInt(0,n-1)
-
-
-
-    for(i in startThinker .. n){
-        if (i == n){
-            n = startThinker
-            startThinker = 0
-        }
-
-        val choice = Random.nextBoolean()
-        var current = i
-        //  есть 1 есть 2 выбрали 1
-        //  не есть 1 есть 2 выбрали 1
-        //  есть 1 не есть 2 выбрали 1
-        //  есть 1 есть 2 не выбрали 1
-        //  есть 1 есть 2 выбрали 1
-        //  есть 1 есть 2 выбрали 1
-        //  есть 1 есть 2 выбрали 1
-
-
-        if (!(choice && forks[i])){
-            current -= 1
-            if (!forks[current]){
-                current += 1
-            }
-        }
-
-        if(forks[current]){
-            thinkers[i].status = Status.EAT
-        }
-        else{
-            thinkers[i].status = Status.THINK
-        }
-
-
-        val chosenFork = when(choice) {
-            true -> {
-                forks[i]
-            }
-            false -> {
-                forks[i-1]
-            }
-        }
-
-
-
-
-        if (choice){
-            if (forks[i]) {
-                forks[i] = false
-                thinkers[i].status = Status.EAT
-            }
-            else if (forks[i-1]) {
-                forks[i-1] = false
-                thinkers[i].status = Status.EAT
-            }
-            else{
-                thinkers[i].status = Status.THINK
-            }
-        }
-        else {
-            if (forks[i-1]) {
-                forks[i-1] = false
-                thinkers[i].status = Status.EAT
-            }
-            else if (forks[i]) {
-                forks[i] = false
-                thinkers[i].status = Status.EAT
-            }
-            else{
-                thinkers[i].status = Status.THINK
-            }
-        }
-
-        thinkers[i]
+    repeat(n) {
+            chooseFork(getRandThinker(n,thinkers),forks,thinkers)
     }
 
-//4-5-6-1-2-3
-    //1
-    //6
+    var thinkNum = 0
+    var eatNum = 0
+
+    thinkers.forEach {
+        if(it.status == Status.THINK)
+            thinkNum++
+        else if (it.status == Status.EAT)
+            eatNum++
+    }
+    println("Кол-во философов которые едят: $eatNum \nКол-во философов которые думают: $thinkNum")
+}
+
+fun getRandThinker(n: Int, thinkers:List<Thinker> ): Int {
+    var currThinker: Int
+    while (true){
+        currThinker = Random.nextInt(0,n)
+        if (thinkers[currThinker].status == Status.WAIT)
+            break
+    }
+    return currThinker
+}
+
+fun chooseFork(i: Int, forks:MutableList<Boolean>, thinkers:List<Thinker>) {
+    val prevFork = if (i == 0) forks.size - 1 else i - 1
+    val forkIndices = if (Random.nextBoolean()) listOf(i, prevFork) else listOf(prevFork, i)
+
+    val chosenFork = forkIndices.firstOrNull { forks[it] }
+    if (chosenFork != null) {
+        forks[chosenFork] = false
+        thinkers[i].status = Status.EAT
+    } else {
+        thinkers[i].status = Status.THINK
+    }
+
+//    val choice = Random.nextBoolean()
+//    var prevFork = i-1
+//    if(i == 0){
+//       prevFork = forks.size-1
+//    }
+//    if (choice) {
+//        if (forks[i]) {
+//            forks[i] = false
+//            thinkers[i].status = Status.EAT
+//        } else if (forks[prevFork]) {
+//            forks[prevFork] = false
+//            thinkers[i].status = Status.EAT
+//        } else {
+//            thinkers[i].status = Status.THINK
+//        }
+//    } else {
+//        if (forks[prevFork]) {
+//            forks[prevFork] = false
+//            thinkers[i].status = Status.EAT
+//        } else if (forks[i]) {
+//            forks[i] = false
+//            thinkers[i].status = Status.EAT
+//        } else {
+//            thinkers[i].status = Status.THINK
+//        }
+//    }
 }
